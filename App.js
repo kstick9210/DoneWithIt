37f6a1f14f31a5bdaa55f7cc520583184f6f76cc
Kathleen Stickel <kstick9210@outlook.com>
2020-10-23 17:24:55 -0500

    create ViewImageScreen

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import WelcomeScreen from './screens/WelcomeScreen';
+import ViewImageScreen from './screens/ViewImageScreen';
 
 export default function App() {
   return (
-    <WelcomeScreen />
+    <ViewImageScreen />
   );
 }
