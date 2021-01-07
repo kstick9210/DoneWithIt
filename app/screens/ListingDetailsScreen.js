66b73e41f047b322dc652db6809780d12f12f8c8
Kathleen Stickel <kstick9210@outlook.com>
2021-01-06 19:13:22 -0600

    create list item comp

--- app/screens/ListingDetailsScreen.js
+++ app/screens/ListingDetailsScreen.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View, Image, StyleSheet } from 'react-native';
 import AppText from '../components/AppText';
+import ListItem from '../components/ListItem';
 import colors from '../config/colors';
 
 export default function ListingDetailsScreen() {
@@ -10,6 +11,13 @@ export default function ListingDetailsScreen() {
             <View style={styles.detailsContainer}>
                 <AppText style={styles.title}>Red jacket for sale</AppText>
                 <AppText style={styles.price}>$100</AppText>
+                <View style={styles.userContainer}>
+                    <ListItem 
+                        image={require("../assets/mosh.jpg")}
+                        title="Mosh Hamedani"
+                        subTitle="5 Listings"
+                    />
+                </View>
             </View>
         </View>
     )
@@ -33,4 +41,7 @@ const styles = StyleSheet.create({
         fontSize: 20,
         marginVertical: 10,
     },
+    userContainer: {
+        marginVertical: 40,
+    }
 })
