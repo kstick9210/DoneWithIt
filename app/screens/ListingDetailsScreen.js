25cfa1eef06dc9e335f93043cf2ce8a1da34fe98
Kathleen Stickel <kstick9210@outlook.com>
2021-01-06 19:01:33 -0600

    create listing details screen

---
+++ app/screens/ListingDetailsScreen.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { View, Image, StyleSheet } from 'react-native';
+import AppText from '../components/AppText';
+import colors from '../config/colors';
+
+export default function ListingDetailsScreen() {
+    return (
+        <View>
+            <Image style={styles.image} source={require('../assets/jacket.jpg')}/>
+            <View style={styles.detailsContainer}>
+                <AppText style={styles.title}>Red jacket for sale</AppText>
+                <AppText style={styles.price}>$100</AppText>
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    image: {
+        width: '100%',
+        height: 300,
+    },
+    detailsContainer: {
+        padding: 20,
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: '500',
+    },
+    price: {
+        color: colors.secondary,
+        fontWeight: 'bold',
+        fontSize: 20,
+        marginVertical: 10,
+    },
+})
