35d4c3f940c09ef3c4ea1d2a2ec14d73849db818
Kathleen Stickel <kstick9210@outlook.com>
2021-01-23 18:28:06 -0600

    add icon comp to list item comp

--- app/components/ListItem.js
+++ app/components/ListItem.js
@@ -4,7 +4,7 @@ import Swipeable from 'react-native-gesture-handler/Swipeable'
 import AppText from './AppText';
 import colors from "../config/colors";
 
-export default function ListItem({title, subTitle, image, onPress, renderRightActions }) {
+export default function ListItem({title, subTitle, image, ImageComponent, onPress, renderRightActions }) {
     return (
         <Swipeable renderRightActions={renderRightActions}>
             <TouchableHighlight 
@@ -12,10 +12,11 @@ export default function ListItem({title, subTitle, image, onPress, renderRightAc
                 onPress={onPress}
             >
                 <View style={styles.container}>
-                    <Image style={styles.image} source={image}/>
-                    <View>
+                    {ImageComponent}
+                    {image && <Image style={styles.image} source={image}/>}
+                    <View style={styles.detailsContainer}>
                         <AppText style={styles.title}>{title}</AppText>
-                        <AppText style={styles.subtitle}>{subTitle}</AppText>
+                        {subTitle && <AppText style={styles.subtitle}>{subTitle}</AppText>}
                     </View>
                 </View>
             </TouchableHighlight>
@@ -26,13 +27,16 @@ export default function ListItem({title, subTitle, image, onPress, renderRightAc
 const styles = StyleSheet.create({
     container: {
         flexDirection: 'row',
-        padding: 15
+        padding: 15,
     },
     image: {
         width: 70,
         height: 70,
         borderRadius: 35,
-        marginRight: 10,
+    },
+    detailsContainer: {
+        marginLeft: 10,
+        justifyContent: 'center'
     },
     title: {
         fontWeight: '500',
