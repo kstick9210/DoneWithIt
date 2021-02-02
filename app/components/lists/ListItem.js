2ec6257e7ac75d2ec73d13a8e08b7beeb1dc1a20
kstick9210 <kstick9210@outlook.com>
2021-02-02 14:59:31 -0600

    add chevrons to message list

--- app/components/lists/ListItem.js
+++ app/components/lists/ListItem.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { View, StyleSheet, Image, TouchableHighlight } from 'react-native'
 import Swipeable from 'react-native-gesture-handler/Swipeable'
+import { MaterialCommunityIcons } from '@expo/vector-icons'
 
 import AppText from '../AppText';
 import colors from "../../config/colors";
@@ -26,6 +27,7 @@ export default function ListItem({
                         <AppText style={styles.title}>{title}</AppText>
                         {subTitle && <AppText style={styles.subtitle}>{subTitle}</AppText>}
                     </View>
+                    <MaterialCommunityIcons name="chevron-right" size={25} color={colors.medium} />
                 </View>
             </TouchableHighlight>
         </Swipeable>
@@ -35,6 +37,7 @@ export default function ListItem({
 const styles = StyleSheet.create({
     container: {
         flexDirection: 'row',
+        alignItems: 'center',
         padding: 15,
         backgroundColor: colors.white
     },
@@ -45,7 +48,8 @@ const styles = StyleSheet.create({
     },
     detailsContainer: {
         marginLeft: 10,
-        justifyContent: 'center'
+        justifyContent: 'center',
+        flex: 1
     },
     title: {
         fontWeight: '500',
