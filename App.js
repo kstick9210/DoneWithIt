387e7498bad7d6c7a9e0b3e38141923967c2d42b
kstick9210 <kstick9210@outlook.com>
2021-03-22 15:10:04 -0500

    image input list formatting

--- App.js
+++ App.js
@@ -3,37 +3,26 @@ import { Button, Image } from 'react-native';
 import * as ImagePicker from 'expo-image-picker'
 
 import Screen from './app/components/Screen'
-import ImageInput from './app/components/ImageInput';
+import ImageInputList from './app/components/ImageInputList';
 
 
 export default function App() {
-  const [imageUri, setImageUri] = useState();
+  const [imageUris, setImageUris] = useState([]);
 
-  const requestPermission = async () => {
-    // const result = await Permissions.askAsync(Permissions.CAMERA_ROLL, Permissions.LOCATION) // same as below
-    const { granted } = await ImagePicker.requestCameraRollPermissionsAsync();
-    if (!granted) alert('You need to enable permission to access the library')
+  const handleAdd = uri => {
+    setImageUris([...imageUris, uri]);
   }
 
-  useEffect(() => {
-    requestPermission();
-  }, [])
-
-  const selectImage = async () => {
-    try {
-      const result = await ImagePicker.launchImageLibraryAsync()
-      if (!result.cancelled)
-        setImageUri(result.uri);
-    } catch (error) {
-      console.log('Error reading an image', error)
-    }
+  const handleRemove = uri => {
+    setImageUris(imageUris.filter(imageUri => imageUri !== uri))
   }
 
   return (
     <Screen>
-      <ImageInput 
-        imageUri={imageUri}
-        onChangeImage={uri => setImageUri(uri)}
+      <ImageInputList 
+        imageUris={imageUris}
+        onAddImage={handleAdd}
+        onRemoveImage={handleRemove}
       />
     </Screen>
   );
