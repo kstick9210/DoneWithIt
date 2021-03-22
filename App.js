b80a30fc9caf86f646487aeda9ec9c025b3bcd9f
kstick9210 <kstick9210@outlook.com>
2021-03-22 14:54:36 -0500

    create image input comp

--- App.js
+++ App.js
@@ -1,24 +1,40 @@
-import React, {useState} from 'react';
-import WelcomeScreen from './app/screens/WelcomeScreen';
-import ViewImageScreen from './app/screens/ViewImageScreen';
-import { View, TextInput, Text } from 'react-native';
-import AppButton from './app/components/AppButton';
-import Card from './app/components/Card'
-import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
-import MessagesScreen from './app/screens/MessagesScreen';
+import React, { useState, useEffect } from 'react';
+import { Button, Image } from 'react-native';
+import * as ImagePicker from 'expo-image-picker'
+
 import Screen from './app/components/Screen'
-import Icon from './app/components/Icon'
-import AccountScreen from './app/screens/AccountScreen';
-import ListingsScreen from './app/screens/ListingsScreen';
-import AppTextInput from './app/components/AppTextInput';
-import AppPicker from './app/components/AppPicker';
-import LoginScreen from './app/screens/LoginScreen';
-import ListingEditScreen from './app/screens/ListingEditScreen';
+import ImageInput from './app/components/ImageInput';
 
 
 export default function App() {
+  const [imageUri, setImageUri] = useState();
+
+  const requestPermission = async () => {
+    // const result = await Permissions.askAsync(Permissions.CAMERA_ROLL, Permissions.LOCATION) // same as below
+    const { granted } = await ImagePicker.requestCameraRollPermissionsAsync();
+    if (!granted) alert('You need to enable permission to access the library')
+  }
+
+  useEffect(() => {
+    requestPermission();
+  }, [])
+
+  const selectImage = async () => {
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync()
+      if (!result.cancelled)
+        setImageUri(result.uri);
+    } catch (error) {
+      console.log('Error reading an image', error)
+    }
+  }
 
   return (
-    <ListingEditScreen />
+    <Screen>
+      <ImageInput 
+        imageUri={imageUri}
+        onChangeImage={uri => setImageUri(uri)}
+      />
+    </Screen>
   );
 }
