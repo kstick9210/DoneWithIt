c3c84d242078ced0bd3b1ef38beea7c17d9df9f6
kstick9210 <kstick9210@outlook.com>
2021-03-22 15:15:56 -0500

    style image input list

--- app/components/ImageInputList.js
+++ app/components/ImageInputList.js
@@ -1,20 +1,30 @@
-import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import React, { useRef } from 'react';
+import { View, StyleSheet, ScrollView } from 'react-native';
 
 import ImageInput from './ImageInput';
 
 export default function ImageInputList({ imageUris = [], onRemoveImage, onAddImage }) {
+    const scrollView = useRef();
+    
     return (
-        <View style={styles.container}>
-            {imageUris.map(uri => (
-                <View key={uri} style={styles.image}>
-                    <ImageInput 
-                        imageUri={uri}
-                        onChangeImage={() => onRemoveImage(uri)}
-                    /> 
+        <View>
+            <ScrollView 
+                horizontal
+                ref={scrollView}
+                onContentSizeChange={() => scrollView.current.scrollToEnd()}
+            >
+                <View style={styles.container}>
+                    {imageUris.map(uri => (
+                        <View key={uri} style={styles.image}>
+                            <ImageInput 
+                                imageUri={uri}
+                                onChangeImage={() => onRemoveImage(uri)}
+                            /> 
+                        </View>
+                    ))}
+                    <ImageInput onChangeImage={uri => onAddImage(uri)} />
                 </View>
-            ))}
-            <ImageInput onChangeImage={uri => onAddImage(uri)} />
+            </ScrollView>
         </View>
     )
 }
