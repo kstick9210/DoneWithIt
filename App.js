4033bd9eb703f45329a79b4841c4486bba2c1e40
kstick9210 <kstick9210@outlook.com>
2021-03-23 10:08:37 -0500

    add basic navigation

--- App.js
+++ App.js
@@ -1,11 +1,35 @@
 import React from 'react';
+import { Text } from 'react-native'
+import { createStackNavigator } from '@react-navigation/stack'
+import { NavigationContainer } from '@react-navigation/native'
 
-import ListingEditScreen from './app/screens/ListingEditScreen';
+import Screen from './app/components/Screen'
 
+
+const Tweets = () => (
+  <Screen>
+    <Text>Tweets</Text>
+  </Screen>
+)
+
+const TweetDetails = () => (
+  <Screen>
+    <Text>Tweet Details</Text>
+  </Screen>
+)
 
 export default function App() {
+  const Stack = createStackNavigator(); // Stack is an object with properties that are React components
+  const StackNavigator = () => (
+    <Stack.Navigator initialRouteName="Tweets">
+      <Stack.Screen name="TweetDetails" component={TweetDetails}/>
+      <Stack.Screen name="Tweets" component={Tweets}/>
+    </Stack.Navigator>
+  )
 
   return (
-    <ListingEditScreen />
+    <NavigationContainer>
+      <StackNavigator />
+    </NavigationContainer>
   );
 }
