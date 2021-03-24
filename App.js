5bfedde587146dd0ee38b6733794fa423c4d8baf
kstick9210 <kstick9210@outlook.com>
2021-03-24 09:53:20 -0500

    dynamic routes and route ids

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
 import React from 'react';
-import { Text } from 'react-native'
+import { Text, Button } from 'react-native'
 import { createStackNavigator } from '@react-navigation/stack'
-import { NavigationContainer } from '@react-navigation/native'
+import { NavigationContainer, useNavigation } from '@react-navigation/native'
 
 import Screen from './app/components/Screen'
 
+const Link = () => {
+  const navigation = useNavigation(); //how to access navigation from a component (not avil from props in comp)
 
-const Tweets = () => (
+  return (
+    <Button 
+      title="Click"
+      onPress={() => navigation.navigate("TweetDetails")}
+    />
+  )
+}
+
+const Tweets = ({ navigation }) => ( //navigation prop only avail on Screen comps (ones we declare as Stack.Screen in the Stack Navigator)
   <Screen>
     <Text>Tweets</Text>
+    <Button 
+      title="View Tweet"
+      onPress={() => navigation.navigate("TweetDetails", {id: 1 })} //navigate method ensures there is only one instance of this route on the navigation stack
+    />
+    {/* <Link /> */}
   </Screen>
 )
 
-const TweetDetails = () => (
+const TweetDetails = ({ route }) => (
   <Screen>
-    <Text>Tweet Details</Text>
+    <Text>Tweet Details {route.params.id}</Text>
   </Screen>
 )
 
@@ -22,7 +37,11 @@ export default function App() {
   const Stack = createStackNavigator(); // Stack is an object with properties that are React components
   const StackNavigator = () => (
     <Stack.Navigator initialRouteName="Tweets">
-      <Stack.Screen name="TweetDetails" component={TweetDetails}/>
+      <Stack.Screen 
+        name="TweetDetails" 
+        component={TweetDetails}
+        options={({ route }) => ({ title: route.params.id })} //dynamically set title of page
+      />
       <Stack.Screen name="Tweets" component={Tweets}/>
     </Stack.Navigator>
   )
